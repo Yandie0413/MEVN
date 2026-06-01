8124523839a3de86a409152afc4b96c2b5d332c5
Yandie0413 <yandiepassot@gmail.com>
2026-06-01 15:38:09 +0200

    authentification terminée et testée

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const { register, login } = require('../controllers/authController')
-
+const { protect, adminOnly } = require('../middleware/authMiddleware')
 router.post('/register', register)
 router.post('/login', login)
+//routes  protégé
+router.get('/profile', protect, (req, res) =>{
+    res.json({ message: 'Bienvenue sur ton profil', user: req.user})
+})
+router.get('/admin', protect, adminOnly, (req, res) => {
+    res.json({ message:'Bienvenue admin !'})
+})
 
 module.exports = router
